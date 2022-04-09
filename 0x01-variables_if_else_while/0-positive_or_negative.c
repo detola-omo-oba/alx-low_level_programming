1ce9e2de60a984414639cd7e16bd2186e7e2187c
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-04-09 03:06:40 -0700

    biko

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,6 +7,7 @@
  *
  * Return: Always 0 (Success/correct)
  */
+
 int main(void)
 {
 	int n;
@@ -14,20 +15,21 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n==0)
+	if (n == 0)
 	{
-		printf(“%i is zero\n”, n);
+		printf("%i is zero\n", n);
 	}
 
-		else if (n<0)
-		{
-		printf(“%i is negative\n”, n);
-		}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
 	
 	else
-	{	
-		printf(“%i is positive\n”, n);
+	{
+		printf("%i is positive\n", n);
 	}
 
 	return (0);
+
 }
