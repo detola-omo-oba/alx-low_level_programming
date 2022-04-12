870e938dc0c4eb2bf68058649efc4b89320e6733
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-04-12 13:54:54 -0700

    ok

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	void print_alphabet(void);
+
+	for (print_alphabet = a; print_alphabet <= z; print_alphabet++)
+
+	{
+		putchar(print_alphabet);
+	}
+
+	putchar("\n");
+
+	return (0);
+
+}
