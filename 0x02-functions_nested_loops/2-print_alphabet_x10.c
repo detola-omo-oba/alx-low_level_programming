02e189309be418d017423a28faa2a0dd0ce75edf
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-04-12 15:19:00 -0700

    ok

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print - print alphabet
+ * print_alphabet_x10 - print alphabet
  *
  * Return: Always 0
  */
@@ -10,7 +10,7 @@ void print_alphabet_x10(void);
 	int alphabet;
 	int count;
 
-	count = 0
+	count = 0;
 		while (count < 10)
 		{
 			for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
