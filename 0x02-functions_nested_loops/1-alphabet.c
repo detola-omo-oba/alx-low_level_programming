b0b0a1737670a137ace6472cc63a26241b239d84
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-04-12 14:04:08 -0700

    ok

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@
 int main(void)
 
 {
-	char alphabet;
+	int alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 
@@ -17,7 +17,7 @@ int main(void)
 		putchar(alphabet);
 	}
 
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 
