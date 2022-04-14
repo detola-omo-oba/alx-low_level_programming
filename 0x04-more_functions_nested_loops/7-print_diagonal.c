c84e9f69a59ee9a802b0c2f8a37d0383abb64ae0
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-04-14 16:55:57 -0700

    ok

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,23 +4,25 @@
  * @n: print int
  * Return: Always 0
  */
-void print_diagonal(int n)
+void print_diagonal (int n)
 {
+
 	int i;
+
 	int b;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
-				{
-				for (b = 0; b < i; b++)
-				{
-					_putchar (' ');
-				}
-				_putchar (92);
-				_putchar ('\n');
+		{
+			for (b = 0; b < i; b++)
+			{
+				_putchar (' ');
+			}
+			_putchar (92);
+			_putchar ('\n');
 
-				}
+		}
 
 	}
 
