0191d188c4ebc3dda7f3404f23bbff057e29d5ed
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-04-22 09:09:09 -0700

    ok

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,4 +17,13 @@ char *leet(char *string);
 	for (a = 0; string[a] != '\0'; a++)
 	{
 		for (b = 0; lower[b] != '\0' && upper[b] != '\0'; b++)
-
+		{
+			if (string[a] == lower[b] || string[a] == upper[b])
+			{
+				string[a] = number[b];
+				break;
+			}
+		}
+	}
+	return (string);
+}
