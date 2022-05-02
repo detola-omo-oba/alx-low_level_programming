2336e7f19aed6f696976dbd85b961a5372652090
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-02 08:10:34 -0700

    ok

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: count for args
+ * @argv: array of char
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	 if (!*argv)
+	 return (-1)
+	 printf("%d\n", argc -1);
+	 return (0);
+}
