1690a90c2ce56e8e3d423ee5b1e5e42d022bee69
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-02 08:25:54 -0700

    ok

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: count of args
+ * @argv: array of char
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, product = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+		product *= atoi(argv[i]);
+	printf("%d\n", product);
+	return (0);
+}
