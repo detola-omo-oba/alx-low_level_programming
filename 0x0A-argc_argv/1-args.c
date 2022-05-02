9c2bea4ae7b302d30c90895ed391d4b08ac7a558
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-02 08:17:22 -0700

    ok

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,8 @@
 
 int main(int argc, char *argv[])
 {
-	 if (!*argv)
-	 return (-1)
-	 printf("%d\n", argc -1);
-	 return (0);
+	if (!*argv)
+		return (-1);
+	printf("%d\n", argc - 1);
+	return (0);
 }
