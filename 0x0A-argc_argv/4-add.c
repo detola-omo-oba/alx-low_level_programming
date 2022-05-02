8565e1ec30333efd94bac5436c91764790fa90ea
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-02 08:31:00 -0700

    ok

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry ppoint
+ * @argc: count of args
+ * @argv: array of char
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
