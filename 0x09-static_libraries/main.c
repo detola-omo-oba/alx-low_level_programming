4a00bb031f0db5365b3cecc35c330b77035311ed
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-05 14:55:45 -0700

    ok

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-/**
- * _puts - yes you
- * main - void
- * Return: none
- */
-
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
