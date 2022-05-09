6692be7c3fa196c590871ab5af4c2a530c79770d
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-09 09:50:51 -0700

    ok

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array
+ * @min: smallest number
+ * @max: largest number
+ * Return: pointer to address
+ */
+
+int *array_range(int min, int max)
+{
+	int *block;
+	int i, j = 0;
+
+	if (min > max)
+		return (NULL);
+	block = malloc(sizeof(*block) * ((max - min) + 1));
+	if (block != NULL)
+	{
+		for (i = min; i <= max; i++)
+		{
+			block[j] = i;
+			j++;
+		}
+		return (block);
+	}
+	else
+		return (NULL);
+}
