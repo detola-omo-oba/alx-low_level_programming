908bba4b085f49cdc125380a08347c8f6423a14c
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-09 12:49:37 -0700

    ok

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,33 +12,29 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *temp_block;
-	unsigned int i;
+	char *temp, *tempP;
+	unsigned int i = 0;
 
+	if (new_size == old_size)
+		return (ptr);
 	if (ptr == NULL)
 	{
-		temp_block = malloc(new_size);
-		return (temp_block);
-	}
-	else if (new_size == old_size)
+		ptr = malloc(new_size);
+		if (!ptr)
+			return (NULL);
 		return (ptr);
-
-	else if (new_size == 0 && ptr != NULL)
+	}
+	if (!new_size && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else
-	{
-		temp_block = malloc(new_size);
-		if (temp_block != NULL)
-		{
-			for (i = 0; i < min(old_size, new_size); i++)
-				*((char *)temp_block + i) = *((char *) ptr + i);
-			free(ptr);
-			return (temp_block);
-		}
-		else
-			return (NULL);
-	}
+	temp = malloc(new_size);
+	tempP = ptr;
+	if (old_size > new_size)
+		old_size = new_size;
+	for (i = 0; i < old_size; i++)
+		temp[i] = tempP[i];
+	free(ptr);
+	return (temp);
 }
