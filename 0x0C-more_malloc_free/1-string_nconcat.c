3e6f0ac71e44dd0ab154390717b2dfe222073240
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-09 09:29:11 -0700

    ok

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,18 +1,41 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - check the code
- * Return: Always 0
+ * string_nconcat - concatenates two strings
+ * @s1: first str
+ * @s2: second str
+ * @n: maximum number of bytes
+ * Return: Null
  */
 
-int main(void)
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 {
 	char *concat;
+	unsigned int len = n, index;
 
-	concat = string_nconcat("Best ", "School !!!", 6);
-	printf("%s\n", concat);
-	free(concat);
-	return (0);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
+
 }
