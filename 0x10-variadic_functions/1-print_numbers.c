5a01bf8b14a226680ac670784b856799609cfb55
detola-omo-oba <ayodeleadetolarosemary@gmail.com>
2022-05-15 23:26:19 -0700

    ok

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed
+ * @n: number of integer
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+	va_list li;
+	unsigned int i;
+
+	va_start(li, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(li, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(li);
+
+}
