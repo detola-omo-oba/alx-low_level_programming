f5c36703c2143e7d479463c6e59be87e972052aa
detola-omo-oba <101400387+detola-omo-oba@users.noreply.github.com>
2022-07-07 22:51:02 +0100

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,14 +1,43 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: pointer to head of list
- * @idx: index of the list where the new node should be added
- * @n: data of the new node
- * 
- * Return:  the address of the new node, or NULL if it failed
+ * *insert_dnodeint_at_index - function inserts new node at given position
+ *
+ *@h: pointer to pointer of head of linked list
+ *@idx: unsigned int index position
+ *@n: int data
+ *
+ * Return: address of new element or NULL if failed
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *currentnode;
+	dlistint_t *newnode;
+
+	while (*h)
+	{
+		newnode = malloc(sizeof(dlistint_t));
+		currentnode = *h;
+		newnode->n = n;
+
+		if (idx == 0)
+		{
+			newnode->next = NULL;
+			return (newnode);
+		}
+		while (currentnode->next != NULL && idx - 1 > 0)
+		{
+			currentnode = currentnode->next;
+			idx--;
+		}
+		if (currentnode->next == NULL)
+		{
+			free(newnode);
+			return (NULL);
+		}
+		newnode->next = currentnode->next;
+		currentnode->next = newnode;
+		return (newnode);
+	}
+	return (NULL);
 }
