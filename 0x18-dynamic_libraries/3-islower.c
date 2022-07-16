fa289e5b67456167d342c22b99bce430865555af
detola-omo-oba <101400387+detola-omo-oba@users.noreply.github.com>
2022-07-16 22:37:49 +0100

    done

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check if a letter
+ * is lowercase or not
+ * @c: character to be checked
+ *
+ * Return: integer 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
