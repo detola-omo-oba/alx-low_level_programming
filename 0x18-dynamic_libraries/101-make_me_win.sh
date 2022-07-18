5df488224f26d77200ced4dc0771f5191022b2a0
detola-omo-oba <101400387+detola-omo-oba@users.noreply.github.com>
2022-07-18 21:28:37 +0100

    done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/debbie-ana/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -q -P /tmp https://github.com/adrienmillot/holbertonschool-low_level_programming/raw/main/0x18-dynamic_libraries/hack.so
+export LD_PRELOAD=/tmp/hack.so
